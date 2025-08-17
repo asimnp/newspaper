bb18ceb273364a239fc06a1bddccc6b221010740
asim <asimshrestha608@gmail.com>
2025-08-17 14:37:03 +0545

    test: signup page

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 
 class UserManagerTests(TestCase):
@@ -26,3 +27,28 @@ class UserManagerTests(TestCase):
         self.assertTrue(super_user.is_active)
         self.assertTrue(super_user.is_staff)
         self.assertTrue(super_user.is_superuser)
+
+
+class SignUpViewTests(TestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/accounts/signup/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_page(self):
+        response = self.client.get(reverse("signup"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "registration/signup.html")
+
+        response = self.client.post(
+            reverse("signup"),
+            {
+                "username": "test1",
+                "email": "test1@email.com",
+                "password1": "random-123",
+                "password2": "random-123",
+            },
+        )
+        self.assertRedirects(response, reverse("login"), status_code=302)
+        new_user = get_user_model().objects.last()
+        self.assertEqual(new_user.username, "test1")
+        self.assertEqual(new_user.email, "test1@email.com")
