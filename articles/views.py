6b9287585e18dc10180d3c88765538cb88d69eb9
asim <asimshrestha608@gmail.com>
2025-08-18 14:32:51 +0545

    fix: user can only edit or delete own article

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from .models import Article
 
@@ -16,16 +16,24 @@ class ArticleDetailView(LoginRequiredMixin, DetailView):
     template_name = "articles/detail.html"
 
 
-class ArticleEditView(LoginRequiredMixin, UpdateView):
+class ArticleEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     template_name = "articles/edit.html"
     fields = ("title", "body", "publish")
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
-class ArticleDeleteView(LoginRequiredMixin, DeleteView):
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = "articles/delete.html"
     success_url = reverse_lazy("articles:list")
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
 
 
 class ArticleCreateView(LoginRequiredMixin, CreateView):
