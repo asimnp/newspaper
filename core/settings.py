253cf130d2d0bb19cac07a1a8d5f351efcf01977
asim <asimshrestha608@gmail.com>
2025-08-18 09:36:16 +0545

    ui: add bootstrap5 & crispy-forms

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd party
+    "crispy_forms",
+    "crispy_bootstrap5",
+    # local
     "accounts.apps.AccountsConfig",
     "pages.apps.PagesConfig",
 ]
@@ -127,3 +131,6 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 
 LOGIN_REDIRECT_URL = "pages:home"
 LOGOUT_REDIRECT_URL = "pages:home"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
