c135fa87f8e83bce1c4fe23dd5112070cb9ddd43
asim <asimshrestha608@gmail.com>
2025-08-18 17:20:23 +0545

    setup postgresql

--- core/settings.py
+++ core/settings.py
@@ -87,10 +87,7 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": env.dj_db_url("DATABASE_URL"),
 }
 
 
