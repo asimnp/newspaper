eaf1224f041cec26a579cfe3ae73cbdcf6a81113
asim <asimshrestha608@gmail.com>
2025-08-18 10:45:39 +0545

    model: create article model & register to admin site

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Article
+
+
+@admin.register(Article)
+class Article(admin.ModelAdmin):
+    list_display = ["title", "author", "publish", "created"]
+    list_filter = ["author", "publish", "created"]
+    prepopulated_fields = {"slug": ("title",)}
+    search_fields = ["title", "body"]
+    raw_id_fields = [
+        "author",
+    ]
+    show_facets = admin.ShowFacets.ALWAYS
--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
+from django.utils.text import Truncator
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255, unique=True)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="articles"
+    )
+
+    class Meta:
+        ordering = ["-created"]
+        indexes = [
+            models.Index(fields=["-created"]),
+            models.Index(fields=["-publish"]),
+        ]
+
+    def __str__(self):
+        return f"{Truncator(self.title).chars(50,  truncate="...")}"
