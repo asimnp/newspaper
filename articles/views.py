32e482e06c618f702a50ece8731fc5a3e331e64a
asim <asimshrestha608@gmail.com>
2025-08-18 14:24:38 +0545

    feat: make views only access by authenticated users

--- articles/views.py
+++ articles/views.py
@@ -1,33 +1,34 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Article
 
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = "articles/list.html"
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = "articles/detail.html"
 
 
-class ArticleEditView(UpdateView):
+class ArticleEditView(LoginRequiredMixin, UpdateView):
     model = Article
     template_name = "articles/edit.html"
     fields = ("title", "body", "publish")
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
     model = Article
     template_name = "articles/delete.html"
     success_url = reverse_lazy("articles:list")
 
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = "articles/create.html"
     fields = ("title", "body")
